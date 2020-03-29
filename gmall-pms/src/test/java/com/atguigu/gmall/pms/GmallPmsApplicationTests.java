44a2834eeccaedca4c07f102469270c31f3b060f
孙思杰 <18538107050@163.com>
2020-03-29 22:53:59 +0800

    "elk dubbo zookeeper搭建完成"

--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,35 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+    @Autowired
+    BrandService brandService;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+//        Product byId = productService.getById(1);
+//        System.out.println(byId.getName());
+//        Brand brand = new Brand();
+//        brand.setName("gggggg");
+//        brandService.save(brand);
+//        System.in.read();
+        System.out.println("ssss");
     }
 
 }
