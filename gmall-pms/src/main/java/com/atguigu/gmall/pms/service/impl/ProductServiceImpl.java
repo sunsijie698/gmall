0d1d269a254a211599308df10813507875025383
孙思杰 <18538107050@163.com>
2020-03-29 23:07:11 +0800

    "elk dubbo zookeeper搭建完成9"

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/ProductServiceImpl.java
@@ -0,0 +1,22 @@
+package com.atguigu.gmall.pms.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.mapper.ProductMapper;
+import com.atguigu.gmall.pms.service.ProductService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * 商品信息 服务实现类
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-03-28
+ */
+@Service
+@Component
+public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> implements ProductService {
+
+}
